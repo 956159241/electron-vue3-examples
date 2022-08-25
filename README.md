1847d8ac58b10e06f622aab0a5c2e15bdc24d205
张中华 <956159241@qq.com>
2022-08-25 10:33:11 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,2 @@
+## 参考项目
+根据该项目进行扩展: https://github.com/electron-vite/electron-vite-vue
---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="logo-box">
+    <img style="height:140px;" src="./assets/electron.png" >
+    <span/>
+    <img style="height:140px;" src="./assets/vite.svg" >
+    <span/>
+    <img style="height:140px;" src="./assets/vue.png" >
+  </div>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="static-public">
+    Place static files into the <code>/public</code> folder
+    <img style="width:77px;" :src="'./node.png'" >
+
+    {{ res && res.statusText }}
+  </div>
+</template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { onMounted, ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+const res = ref();
+onMounted(() => {
+  try{
+  fetch("https://h5api.zhefengle.cn/index.html", {
+  // fetch("api/index.html", {
+  "headers": {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
+  },
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "omit"
+}).then(response => {
+  console.log(response);
+  res.value = response;
+});
+  }
+  catch(error){
+console.log(error);
+  }
+
+});
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.logo-box {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+}
+.logo-box span {
+  width: 74px;
+}
+.static-public {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.static-public code {
+  background-color: #eee;
+  padding: 2px 4px;
+  margin: 0 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
